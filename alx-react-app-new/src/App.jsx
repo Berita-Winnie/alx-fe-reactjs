7fde83ba3423ccdec4624275a3e1c0ca370e528b
Berita-Winnie <beritawinnie@gmail.com>
2025-02-23 17:57:51 +0300

    consept using state and hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-
+import Counter from './components/Counter';
 function App() {
   
 
   return (
     <>
+      <Counter />
       <WelcomeMessage />
       <Header />
       <MainContent />
