337632b5add570a67f029ba7e8a437a2bf115496
Berita-Winnie <beritawinnie@gmail.com>
2025-03-09 13:14:37 +0300

    feat: Nested Routes

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import {Routes, Route, Link} from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
+
+const Profile = () => {
+    return (
+        <div>
+            <h2>Profile</h2>
+            <Link to="details">Profile Details</Link>
+            <Link to="settings">Settings</Link>
+            <Routes>
+                <Route path="detaiils" element={< ProfileDetails/>} />
+                <Route path="settings" element={< ProfileSettings/>} />
+            </Routes>
+        
+        </div>
+        
+    );
+};
+export default Profile;
