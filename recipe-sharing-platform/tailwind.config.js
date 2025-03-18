5a116a86c1d99947e5cd0cb5e5f5ee3906a5129c
Berita-Winnie <beritawinnie@gmail.com>
2025-03-18 10:32:51 +0300

    task one

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [ "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
