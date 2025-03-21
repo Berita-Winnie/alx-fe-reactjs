e9699e1344a1d0395a848d9e9cde66111a6c6fd5
Berita-Winnie <beritawinnie@gmail.com>
2025-03-21 11:52:40 +0300

    Feat: Task two

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: [ "./public/index.html",
     "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
